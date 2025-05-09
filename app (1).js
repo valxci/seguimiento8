81e02368a92c5b778f36e2b765847bfa9d5509bd
valxci <valentinacastanoibarra@gmail.com>
2025-05-09 16:17:18 -0500

    intento1

---
+++ app (1).js
@@ -0,0 +1,35 @@
+
+// Seleccionar los elemento x id del DOM: ingresar-tarea, boton-agregar y lista-tareas
+
+// Obtener tareas del localStorage
+function obtenerTareasLocalStorage() {
+}
+
+// Guardar tareas en localStorage
+function guardarTareasLocalStorage(tareas) {
+}
+
+// Renderizar la lista de tareas en el DOM
+function mostrarTareas() {
+}
+
+// Marcar la Tarea como completada
+function completarTarea(index) {
+}
+
+// Eliminar la Tarea correspondiente
+function eliminarTarea(index) {
+}
+
+// Crear una nueva Tarea
+function nuevaTarea() {
+}
+  
+// Escuchar el boton Agregar y en el evento click llamar a nuevaTarea
+
+// Escuchar el inputTarea y en el evento keypress con la tecla Enter 
+// llamar a nuevaTarea
+
+// Cargar tareas al iniciar con mostrarTareas
+
+
